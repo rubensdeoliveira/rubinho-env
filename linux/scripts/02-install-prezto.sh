763318996d398781f916238408968380a844e8b4
Rubens <rubensojunior6@gmail.com>
2025-11-26 22:44:13 -0300

    feat: update scripts

--- linux/scripts/02-install-prezto.sh
+++ linux/scripts/02-install-prezto.sh
@@ -17,12 +17,16 @@ fi
 
 echo "Creating Prezto runcom symlinks..."
 # Use Zsh to create symlinks (Zsh syntax required for glob patterns)
-$ZSH_BIN -c "
+TMP_ZSH_SCRIPT=$(mktemp /tmp/prezto_symlinks.XXXXXX)
+cat > "$TMP_ZSH_SCRIPT" << 'ZSH_EOF'
 setopt EXTENDED_GLOB
-for rcfile in \"\${ZDOTDIR:-\$HOME}\"/.zprezto/runcoms/^README.md(.N); do
-  ln -sf \"\$rcfile\" \"\${ZDOTDIR:-\$HOME}/.\${rcfile:t}\"
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -sf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
-"
+ZSH_EOF
+
+$ZSH_BIN "$TMP_ZSH_SCRIPT"
+rm "$TMP_ZSH_SCRIPT"
 
 echo "Writing .zpreztorc (module config)..."
 cat > ~/.zpreztorc << 'EOF'
